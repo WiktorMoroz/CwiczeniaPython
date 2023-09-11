7ff0aab45f84a0f2c25e913a3944571e61474bb4
WiktorMoroz <wiktor.moroz@interia.pl>
2023-09-11 21:08:52 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+ranks = (
+    'Private',
+    'Corporal',
+    'Sergeant',
+    'Lieutenant',
+    'Captain',
+    'Major',
+    'Colonel',
+)
+
+
+num_ranks = len(ranks)
+major_index = ranks.index('Major')
+colonel_present = 'Colonel' in ranks
+print(num_ranks)
+print(major_index)
+print(colonel_present)
